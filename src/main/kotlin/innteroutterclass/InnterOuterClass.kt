b614330b3481375189135a3d3aa6f2016cf7e821
fakhrul <fakhrul.alam@amiprobashi.com>
2022-02-17 15:01:12 +0600

    - inline

--- src/main/kotlin/innteroutterclass/InnterOuterClass.kt
+++ src/main/kotlin/innteroutterclass/InnterOuterClass.kt
@@ -1,4 +1,13 @@
 package innteroutterclass
 
-class InnterOuterClass {
+class Outer {
+    private val bar: Int = 1
+    inner class Nested {
+        fun foo() = 2
+    }
+}
+
+fun main(){
+    var innerclass = Outer().Nested()
+    println(innerclass.foo())// == 2)
 }
